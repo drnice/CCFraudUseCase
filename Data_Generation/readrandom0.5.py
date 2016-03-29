ed9c1ddb5143246d01a3f61f96d970ccaca8f59f
drnice <dan1rice7@gmail.com>
2016-03-29 10:57:53 -0400

    allow for parameters to be passed to script
    added parameter for the two file types to be passed.  If nothing passed
    will take the default zipcode.csv and name_cc.csv files in same
    directory as script.  Still need to add the # of trans and # of
    transactions before a fraud occurs but plan on doing that tonight.
    Script should still be in working order now.

--- Data_Generation/readrandom0.5.py
+++ Data_Generation/readrandom0.5.py
@@ -1,14 +1,46 @@
+#!/usr/bin/python
 #version 0.2 output all in a single line
 #version 0.3 converted to json format
 #version 0.4 added time to the transaction
 #version 0.5 format as GeoJSON (http://geojson.org/geojson-spec.html) for use with mapping APIs
+
+#added parameters to pass to the python script
+#pass filename of name_cc completed
+#pass filename of zipcod completed
+#how many transactions to create in one script execution ***still working on this
+#script executions to fraudulent transactions to make ***stil working on this
+
 import os
 import random
 import sys
 import readline
 import json
 import time
- 
+import getopt
+
+def main(argv):
+    global filezip
+    global filecc
+    global num_of_trans
+    global num_before_fraud
+    try:
+        opts, args = getopt.getopt(argv,"h:c:z:i:f:",["cfile=","zfile=","iterations=","fraud="])
+    except getopt.GetoptError:
+        sys.stdout.write('readrandom0.5.py -c <input_cc_file> -z <input_zip_file> -i <# of transactions> -f <# of transactions before fraud>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            sys.stdout.write('readrandom0.5.py -c <input_cc_file> -z <input_zip_file> -i <# of transactions> -f <# of transactions before fraud>')
+            sys.exit()
+        elif opt in ("-c", "--cfile"):
+            filecc = arg
+        elif opt in ("-z", "--zfile"):
+            filezip = arg
+        elif opt in ("-i", "--interations"):
+            num_of_trans = arg
+        elif opt in ("-f", "--fraud"):
+            num_before_fraud = arg
+
 def get_random_line(file_name):
     total_bytes = os.stat(file_name).st_size
     random_point = random.randint(0, total_bytes)
@@ -17,8 +49,24 @@ def get_random_line(file_name):
     file.readline()
     return file.readline()
 
-linezip=get_random_line(file_name='zipcode.csv').rstrip('\r\n')
-linecc=get_random_line(file_name='name_cc.csv').rstrip('\r\n')
+filezip=''
+filecc=''
+num_of_trans=''
+num_before_fraud=''
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+if filecc =='':
+    filecc = 'name_cc.csv'
+    
+if filezip == '':
+    filezip = 'zipcode.csv'
+
+
+
+linezip=get_random_line(filezip).rstrip('\r\n')
+linecc=get_random_line(filecc).rstrip('\r\n')
 transaction_comma=linecc+','+linezip
 splitstring=transaction_comma.split(',')
 
