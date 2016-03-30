3afd9f6592472a7cc153f7b555e9dcef2bc662cd
drnice <dan1rice7@gmail.com>
2016-03-30 16:39:47 -0400

    Add to more parameter -i -f
    -i is for number of transactions
    -f is for number of transactions before flagging something as fraud

--- Data_Generation/readrandom0.5.py
+++ Data_Generation/readrandom0.5.py
@@ -23,6 +23,7 @@ def main(argv):
     global filecc
     global num_of_trans
     global num_before_fraud
+    global transaction
     try:
         opts, args = getopt.getopt(argv,"h:c:z:i:f:",["cfile=","zfile=","iterations=","fraud="])
     except getopt.GetoptError:
@@ -49,10 +50,32 @@ def get_random_line(file_name):
     file.readline()
     return file.readline()
 
+def writefile(file_trans, num, action):
+    f = open(file_trans, action)
+    f.write(num+'\n')
+    f.close()
+
+def readfile(file_trans, action):
+    global num_of_trans
+    global num_before_fraud
+    loop=0
+    f = open(file_trans, action)
+    for line in f:
+        if loop == 0:
+            num_of_trans=line.rstrip('\r\n')
+        else:
+            num_before_fraud=line.rstrip('\r\n')
+        loop=loop+1
+    f.close()
+
+
+
 filezip=''
 filecc=''
-num_of_trans=''
-num_before_fraud=''
+num_of_trans=0
+num_before_fraud=0
+transaction=''
+flagfraud=0
 
 if __name__ == "__main__":
     main(sys.argv[1:])
@@ -63,13 +86,44 @@ if filecc =='':
 if filezip == '':
     filezip = 'zipcode.csv'
 
+if os.path.isfile('numtrans2fraud'):
+    readfile('numtrans2fraud', 'r')
+    num_of_trans=int(num_of_trans)
+    num_before_fraud=int(num_before_fraud)
+    num_before_fraud= num_before_fraud - num_of_trans
+    if int(num_before_fraud)==0:
+        os.remove('numtrans2fraud')
+        flagfraud=3
+    else:
+        writefile('numtrans2fraud', str(num_of_trans), 'w')
+        writefile('numtrans2fraud', str(num_before_fraud), 'a')
+elif num_of_trans != 0 and num_before_fraud != 0:
+    writefile('numtrans2fraud', num_of_trans, 'w')
+    writefile('numtrans2fraud', num_before_fraud, 'a')
+    writefile('ccfraud','2','w')
+    flagfraud=1
+
+if num_of_trans==0:
+    num_of_trans=1
+for x in range(0, int(num_of_trans)):
+    linezip=get_random_line(filezip).rstrip('\r\n')
+    linecc=get_random_line(filecc).rstrip('\r\n')
+    if os.path.isfile('ccfraud') and flagfraud == 1:
+        writefile('ccfraud',linecc, 'w')
+        flagfraud =2
+    elif os.path.isfile('ccfraud') and flagfraud == 3:
+        f = open('ccfraud', 'r')
+        linecc=f.readline().rstrip('\r\n')
+        f.close
+        os.remove('ccfraud')
+
+    transaction_comma=linecc+','+linezip
+    splitstring=transaction_comma.split(',')
+
+    #transaction={x :{'type':'Feature', 'properties': {'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[6]), float(splitstring[7])]}}}
+
+    transaction={'type':'Feature', 'properties': {'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[6]), float(splitstring[7])]}}
 
 
-linezip=get_random_line(filezip).rstrip('\r\n')
-linecc=get_random_line(filecc).rstrip('\r\n')
-transaction_comma=linecc+','+linezip
-splitstring=transaction_comma.split(',')
-
-transaction={'type':'Feature', 'properties': {'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[6]), float(splitstring[7])]}}
-
-sys.stdout.write(json.dumps(transaction))
+    sys.stdout.write(json.dumps(transaction))
+    sys.stdout.write('\n')
