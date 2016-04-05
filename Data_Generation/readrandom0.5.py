ce392b6791a073d4cb7c812a40c7881fc623dbcc
Ronald McCollam <rmccollam@HW11242.udlunder>
2016-04-05 09:34:15 -0400

    Swap latitude and longitude in point generation

--- Data_Generation/readrandom0.5.py
+++ Data_Generation/readrandom0.5.py
@@ -120,7 +120,7 @@ for x in range(0, int(num_of_trans)):
     transaction_comma=linecc+','+linezip
     splitstring=transaction_comma.split(',')
 
-    #transaction={x :{'type':'Feature', 'properties': {'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[6]), float(splitstring[7])]}}}
+    #transaction={x :{'type':'Feature', 'properties': {'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[7]), float(splitstring[6])]}}}
 
     transaction={'type':'Feature', 'properties': {'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[6]), float(splitstring[7])]}}
 
