4177cee514f994abefd60be0b8330c0113a89589
drnice <dan1rice7@gmail.com>
2016-04-08 11:49:50 -0400

    fraud false/true

--- Data_Generation/readrandom0.5.py
+++ Data_Generation/readrandom0.5.py
@@ -141,7 +141,7 @@ for x in range(0, int(num_of_trans)):
     if int(num_before_fraud)==x:
         transaction={'type':'Feature', 'properties': {'fraud': True, 'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[7]), float(splitstring[6])]}}
     else:
-        transaction={'type':'Feature', 'properties': {'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[7]), float(splitstring[6])]}}
+        transaction={'type':'Feature', 'properties': {'fraud': False, 'date':int(time.time()), 'fname':splitstring[0], 'lname':splitstring[1], 'cc':splitstring[2], 'zip':splitstring[3], 'city':splitstring[4], 'state':splitstring[5], 'cost':random.randint(1,800)}, 'geometry':{'type':'Point', 'coordinates':[float(splitstring[7]), float(splitstring[6])]}}
 
 
     sys.stdout.write(json.dumps(transaction))
